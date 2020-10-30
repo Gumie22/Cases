3b09d73f5e355b9a7082949764de57f3a785a28a
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 16:00:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//This is a C# program that switches the number of the day with the name of the day in the week
+using System;
+
+namespace Cases.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(2));
+        }
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break; //We insert "break" to stop the program once it has switched 
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName; //Returns the day Name requested
+        }
+    }
+}
